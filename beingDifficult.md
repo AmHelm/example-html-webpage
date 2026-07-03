e9e01b13045dab9018afdd52d1524d3191545a2a
AmHelm <amanda.hemlfrid@gmail.com>
2026-07-03 10:31:32 +0200

    Frontend now fetches meme texts from backend and backend reads texts from file.

---
+++ beingDifficult.md
@@ -0,0 +1,9 @@
+Still working on it.
+Yeah no, haven't gotten that far yet :/
+No memes yet. Chill.
+:|
+I'm just the intern, I don't know how to add pictures yet &#128133;
+FINE, here you go... &#128556; uhh... &#128556;
+... Okay, you got me... There are no memes yet
+... Come back tomorrow, K?
+Now this is just awkward for the both of us
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 // Backend program that will serve the static meme webpage
 
-//#![allow(unused)]
+#![allow(unused)]
 
-use axum::Router;
-use tower_http::services::{ServeDir, ServeFile};
+use axum::{Router, 
+           routing::get, 
+           Json};
+use tower_http::services::{ServeDir, 
+                           ServeFile};
 use std::net::SocketAddr;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::io;
+
+// Use reqwest to make GET requests from webpage?
 
 // Which port we want the website to claim
 const PORT: u16 = 3000;
@@ -13,16 +21,60 @@ const PORT: u16 = 3000;
 const DIR_PATH: &str = "meme-original";
 const FILE_NAME: &str = "index.html";
 
+// The name of the text file we want to read
+const MEME_TEXTS: &str = "beingDifficult.md";
+
+
+// Function that reads a file containing strings and returns a list
+// Does not print the emojis correctly, but maybe it's fine for now, 
+// maybe the html reads it correctly later? Otherwise solve later
+// Should add more error handling if file not there etc
+fn read_memes_from_file() -> io::Result<Vec<String>> {
+
+    // Read the file
+    let file = File::open(MEME_TEXTS).unwrap();
+
+    // BufReader lets us read the file line by line
+    let reader = BufReader::new(file);
+
+    // Make a vector to contain the text lines
+    let mut lines: Vec<String>   = Vec::new();
+
+    // Add each text line into a vector
+    for line in reader.lines() {
+        let line = line?;
+        //println!("{line}");
+        lines.push(line);
+    }
+
+    // If no error, return the vector containing all the text lines
+    Ok(lines)
+
+}
+
+// Should send off the meme strings in Json format
+async fn text_memes_to_json() -> Json<Vec<String>> {
+
+    let memes = read_memes_from_file().unwrap();
+    
+    // Wrap the text files into Json format
+    Json(memes)
+}
+
 #[tokio::main]
 async fn main(){
 
     let file_path = format!("{DIR_PATH}/{FILE_NAME}");
 
     // Serve html file in the DIR_NAME directory
-    let serve_dir = ServeDir::new(DIR_PATH).not_found_service(ServeFile::new(file_path));
+    let serve_dir = ServeDir::new(DIR_PATH)
+                                                    .not_found_service(ServeFile::new(file_path));
 
     // Initialize the router
-    let app = Router::new().fallback_service(serve_dir);
+    // Add API here
+    let app = Router::new()
+                      .route("/api/memes", get(text_memes_to_json))
+                      .fallback_service(serve_dir);
 
     // Network adress: IP-adress + port
     // 0.0.0.0 to be reachable from outside
