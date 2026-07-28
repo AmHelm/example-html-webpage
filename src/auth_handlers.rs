e38c2840d7cf66e3406aebfdb1f0770e6a1d50be
AmHelm <amanda.hemlfrid@gmail.com>
2026-07-28 08:09:52 +0200

    Split program into separate modules.

---
+++ src/auth_handlers.rs
@@ -0,0 +1,125 @@
+use axum::{Json, Router, extract::{Extension, Request, State}, 
+            http::StatusCode, middleware::{self, Next}, 
+            response::{IntoResponse, Response}, 
+            routing::{Route, get, post}};
+use tower_http::services::{ServeDir, 
+                            ServeFile};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::fs::{File, 
+                OpenOptions};
+use std::io::{BufReader, 
+                BufRead, Write};
+use std::io;
+use rand::{Rng, distr::Alphanumeric, 
+            seq::IndexedRandom, RngExt};
+use serde::Deserialize;
+use argon2::{Argon2, PasswordHash, 
+                PasswordHasher, PasswordVerifier};
+use argon2::password_hash::{SaltString, rand_core::OsRng};
+use std::sync::{Arc, Mutex};
+use tower_cookies::{Cookie, CookieManagerLayer, Cookies}; 
+
+use crate::AppState;
+
+#[derive(serde::Deserialize)]
+pub struct UserCredentials{
+    username: String,
+    password: String,
+}
+
+// Handler for salting and then hashing password
+// Salting: random chunk of data that is added to a password before hashing it 
+pub fn hash_password(password: &str) -> String {
+    let salt = SaltString::generate(&mut OsRng);
+    Argon2::default()
+        .hash_password(password.as_bytes(), &salt)
+        .unwrap().to_string()
+}
+
+// Handler for verifying password
+// Checks that the stored hash is the same as the submitted password
+pub fn is_valid(password: &str, stored_hash: &str) -> bool {
+    match PasswordHash::new(stored_hash) {
+        Ok(parsed_hash) => Argon2::default()
+            .verify_password(password.as_bytes(), &parsed_hash)
+            .is_ok(),
+        Err(_) => false,
+    }
+}
+
+// Frontend uses this to check if the user is logged in
+pub async fn me(Extension(user): Extension<String>) -> Json<String> {
+    Json(user)   
+}
+
+// Useful for cookies: https://github.com/imbolc/tower-cookies/tree/main
+
+// Checks if user token matches stored tokens
+pub async fn auth(
+    State(state): State<AppState>,
+    cookies: Cookies,
+    mut request: Request,     
+    next: Next,
+) -> Result<Response, (StatusCode, String)> {
+    // 
+    let token = cookies
+        .get("auth_token")
+        .map(|c| c.value().to_string())
+        .ok_or((StatusCode::UNAUTHORIZED, "Not logged in".to_string()))?;
+
+    // Retrieves the username associated with this token
+    let username = state.tokens.lock().unwrap().get(&token).cloned();
+
+    // Checks if token matches a stored user token, either accepts or rejects letting the user through
+    match username {
+        Some(user) => {
+            request.extensions_mut().insert(user); 
+            Ok(next.run(request).await)
+        }
+        None => Err((StatusCode::UNAUTHORIZED, "Invalid token".to_string())), 
+    }
+}
+
+// Login function
+pub async fn login_user(
+    State(state): State<AppState>,
+    cookies: Cookies,
+    Json(body): Json<UserCredentials>) -> Result<StatusCode, (StatusCode, String)> {
+
+    // Message to use if login details are incorrect
+    let error_response: String = "Invalid username or password".to_string();
+
+    // Looks up the stores password for this username
+    let stored_hash = state.users.get(&body.username).ok_or((StatusCode::UNAUTHORIZED, error_response.clone()))?;
+
+    // Passes the submitted password and stored hash to handler for a validity check 
+    if !is_valid(&body.password, stored_hash) {
+        return Err((StatusCode::UNAUTHORIZED, error_response));
+    }
+
+    // Generate a random token
+    let token: String = rand::rng().sample_iter(&Alphanumeric).take(50).map(char::from).collect();
+    
+    // Inserts token into AppState struct
+    state.tokens.lock().unwrap().insert(token.clone(), body.username.clone());
+
+    // Store token as cookie
+    let mut cookie = Cookie::new("auth_token", token);
+    cookie.set_path("/");
+    cookies.add(cookie);
+
+    Ok(StatusCode::OK)
+}
+
+// Handler to log out user
+pub async fn logout_user(State(state): State<AppState>, cookies: Cookies) -> StatusCode {
+    // Read token from "auth_token" cookie
+    // If there is a cookie, remove it from AppState
+    if let Some(cookie) = cookies.get("auth_token") {
+        state.tokens.lock().unwrap().remove(cookie.value());
+    }
+    // Remove cookie from browser
+    cookies.remove(Cookie::new("auth_token", ""));
+    StatusCode::OK
+}
