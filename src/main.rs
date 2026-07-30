b63d63a9948816cc42ac9e6d44f5a6ae9727648c
AmHelm <amanda.helmfrid@gmail.com>
2026-07-30 14:43:24 +0200

    Added separate module for authentication handlers (#19)
    * Split program into separate modules.
    
    * Put authentication handlers in a separate module and updated README.
    
    ---------
    
    Co-authored-by: AmHelm <amanda.hemlfrid@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![allow(unused)]
 #![allow(non_snake_case)]
 
+mod auth_handlers;
+
 use axum::{Json, Router, extract::{Extension, Request, State}, 
             http::StatusCode, middleware::{self, Next}, 
             response::{IntoResponse, Response}, 
@@ -24,7 +26,8 @@ use argon2::{Argon2, PasswordHash,
 use argon2::password_hash::{SaltString, rand_core::OsRng};
 use std::sync::{Arc, Mutex};
 use tower_cookies::{Cookie, CookieManagerLayer, Cookies}; 
-
+use auth_handlers::{hash_password, is_valid, me, 
+                    auth, login_user, logout_user};
 
 // Which port we want the website to claim
 const PORT: u16 = 3000;
@@ -46,12 +49,6 @@ struct NewMeme {
 // This was userful for login auth: https://github.com/wpcodevo/rust-axum-jwt-auth/blob/master/src/handler.rs
 // and for middleware auth: https://docs.rs/axum/latest/axum/middleware/index.html 
 
-#[derive(serde::Deserialize)]
-struct UserCredentials{
-    username: String,
-    password: String,
-}
-
 // Shares data across handlers
 // Arc: lets multiple owners share the same data
 // Mutex: Allows for dafe modification of shared data
@@ -59,102 +56,6 @@ struct UserCredentials{
 struct AppState {
     users: Arc<HashMap<String, String>>,
     tokens: Arc<Mutex<HashMap<String, String>>>,
-}
-
-// Handler for salting and then hashing password
-// Salting: random chunk of data that is added to a password before hashing it 
-fn hash_password(password: &str) -> String {
-    let salt = SaltString::generate(&mut OsRng);
-    Argon2::default()
-        .hash_password(password.as_bytes(), &salt)
-        .unwrap().to_string()
-}
-
-// Handler for verifying password
-// Checks that the stored hash is the same as the submitted password
-fn is_valid(password: &str, stored_hash: &str) -> bool {
-    match PasswordHash::new(stored_hash) {
-        Ok(parsed_hash) => Argon2::default()
-            .verify_password(password.as_bytes(), &parsed_hash)
-            .is_ok(),
-        Err(_) => false,
-    }
-}
-
-// Frontend uses this to check if the user is logged in
-async fn me(Extension(user): Extension<String>) -> Json<String> {
-    Json(user)   
-}
-
-// Useful for cookies: https://github.com/imbolc/tower-cookies/tree/main
-
-// Checks if user token matches stored tokens
-async fn auth(
-    State(state): State<AppState>,
-    cookies: Cookies,
-    mut request: Request,     
-    next: Next,
-) -> Result<Response, (StatusCode, String)> {
-    // 
-    let token = cookies
-        .get("auth_token")
-        .map(|c| c.value().to_string())
-        .ok_or((StatusCode::UNAUTHORIZED, "Not logged in".to_string()))?;
-
-    // Retrieves the username associated with this token
-    let username = state.tokens.lock().unwrap().get(&token).cloned();
-
-    // Checks if token matches a stored user token, either accepts or rejects letting the user through
-    match username {
-        Some(user) => {
-            request.extensions_mut().insert(user); 
-            Ok(next.run(request).await)
-        }
-        None => Err((StatusCode::UNAUTHORIZED, "Invalid token".to_string())), 
-    }
-}
-
-// Login function
-async fn login_user(
-    State(state): State<AppState>,
-    cookies: Cookies,
-    Json(body): Json<UserCredentials>) -> Result<StatusCode, (StatusCode, String)> {
-
-    // Message to use if login details are incorrect
-    let error_response: String = "Invalid username or password".to_string();
-
-    // Looks up the stores password for this username
-    let stored_hash = state.users.get(&body.username).ok_or((StatusCode::UNAUTHORIZED, error_response.clone()))?;
-
-    // Passes the submitted password and stored hash to handler for a validity check 
-    if !is_valid(&body.password, stored_hash) {
-        return Err((StatusCode::UNAUTHORIZED, error_response));
-    }
-
-    // Generate a random token
-    let token: String = rand::rng().sample_iter(&Alphanumeric).take(50).map(char::from).collect();
-    
-    // Inserts token into AppState struct
-    state.tokens.lock().unwrap().insert(token.clone(), body.username.clone());
-
-    // Store token as cookie
-    let mut cookie = Cookie::new("auth_token", token);
-    cookie.set_path("/");
-    cookies.add(cookie);
-
-    Ok(StatusCode::OK)
-}
-
-// Handler to log out user
-async fn logout_user(State(state): State<AppState>, cookies: Cookies) -> StatusCode {
-    // Read token from "auth_token" cookie
-    // If there is a cookie, remove it from AppState
-    if let Some(cookie) = cookies.get("auth_token") {
-        state.tokens.lock().unwrap().remove(cookie.value());
-    }
-    // Remove cookie from browser
-    cookies.remove(Cookie::new("auth_token", ""));
-    StatusCode::OK
 }
 
 // Function that reads a file containing strings and returns a list
